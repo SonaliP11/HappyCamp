b4212a1f2c823385d298c7fe640f359ab9423aed
SonaliP11 <sonali.jain433@gmail.com>
2025-02-13 12:57:02 +0000

    Update view

--- camp/views.py
+++ camp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 
-    return HttpResponse("Welcome to happy camp - group! Testing....")
+    return HttpResponse("Welcome to happy campers")
 
     if request.method == "POST":
         return HttpResponse("You must have POSTed something")
