a8e46ac0d1acdfa4647c7f55a169dfa86e0ef7b6
SonaliP11 <sonali.jain433@gmail.com>
2025-02-13 11:51:56 +0000

    Add sonali in view

--- camp/views.py
+++ camp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to happy camp!")
+    return HttpResponse("Welcome to happy camp! - Sonali")
 
     if request.method == "POST":
         return HttpResponse("You must have POSTed something")
