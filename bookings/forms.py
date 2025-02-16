38e50aa0f09cdf097a8c408cbbfbfb4605cab84a
Prapti <prapti.tambe@gmail.com>
2025-02-16 00:02:45 +0000

    Update booking list to display clubs in list form

--- bookings/forms.py
+++ bookings/forms.py
@@ -3,15 +3,26 @@ from .models import Booking
 from children.models import Child
 from camp.models import Club
 
-# Form for creating a new booking
-# This form is used to create a new booking
 class BookingForm(forms.ModelForm):
+    club = forms.ModelChoiceField(
+        queryset=Club.objects.all(),
+        required=True,
+        empty_label="Please select activity from club",
+        widget=forms.Select()
+    )
+    child = forms.ModelChoiceField(
+        queryset=Child.objects.all(),
+        required=True,
+        empty_label="Please select your child name",
+        widget=forms.Select()
+    )
+
     class Meta:
         model = Booking
         fields = ['club', 'child']
-        
-    def __init__(self, *args, **kwargs):        
+
+    def __init__(self, *args, **kwargs):
         user = kwargs.pop('user')  # Get the logged-in user
         super().__init__(*args, **kwargs)
         # Filter child options to only show the parent's children
-        self.fields['child'].queryset = Child.objects.all().filter(parent=user)
+        self.fields['child'].queryset = Child.objects.filter(parent=user)
