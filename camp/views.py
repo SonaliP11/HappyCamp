e9955424bcde71ca44d53ceb56c6d19121e1aaf9
SonaliP11 <sonali.jain433@gmail.com>
2025-02-17 00:36:31 +0000

    Change font and color style

--- camp/views.py
+++ camp/views.py
@@ -3,38 +3,30 @@ from django.utils import timezone
 from django.views import generic
 from .models import Club
 from datetime import datetime, timedelta
-
 def club_detail(request, slug):
     club = get_object_or_404(Club, slug=slug)
     return render(request, 'camp/club_detail.html', {'club': club})
-
 class ClubList(generic.ListView):
     model = Club
     template_name = "camp/index.html"
-
     def get_queryset(self):
         today = timezone.now().date()
         start_of_week = today - timedelta(days=today.weekday())
         end_of_week = start_of_week + timedelta(days=6)
         start_of_next_week = end_of_week + timedelta(days=1)
         end_of_next_week = start_of_next_week + timedelta(days=6)
-
         this_week_clubs = Club.objects.filter(start_date__range=[start_of_week, end_of_week]).order_by('start_date')
         next_week_clubs = Club.objects.filter(start_date__range=[start_of_next_week, end_of_next_week]).order_by('start_date')
-
         for club in this_week_clubs:
             club.available_spots = club.capacity - club.bookings.count()
             club.available_percentage = (club.available_spots / club.capacity) * 100 if club.capacity > 0 else 0
-
         for club in next_week_clubs:
             club.available_spots = club.capacity - club.bookings.count()
             club.available_percentage = (club.available_spots / club.capacity) * 100 if club.capacity > 0 else 0
-
         return {
             'this_week_clubs': this_week_clubs,
             'next_week_clubs': next_week_clubs,
         }
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(self.get_queryset())
